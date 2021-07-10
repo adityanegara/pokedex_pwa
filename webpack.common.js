ecfe7b712105a60caf76657800350eced952ccd5
adityanegara <aditnegarabisnis@gmail.com>
2021-07-10 16:39:35 +0700

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+     entry : {
+         main : "./src/scripts/index.js",
+         vendor : "./src/scripts/vendor/bootstrap.js"
+     },
+     plugins : [new HtmlWebpackPlugin({
+         template : "./src/templates/index.html"
+     })],
+     module:{
+         rules: [
+           
+            {
+                test : /\.html$/,
+                loader : 'html-loader'
+            },
+            {
+                test: /\.(svg|png|jpg|gif)/,
+                type : 'asset/resource'
+            }
+           
+
+         ]
+    }
+}
